935b0709f233cf6e65a9a838c2a796303867c93e
Jonas Bandi <dev (at) jonasbandi (dot) net>
2015-08-06 23:27:19 +0200

    more

--- app.js
+++ app.js
@@ -3,42 +3,158 @@ FIREBASE_BASE = 'https://scorching-inferno-3523.firebaseio.com';
 var ref = new Firebase("https://scorching-inferno-3523.firebaseio.com");
 
 var btnTweet = $('#tweet'),
+    spnUsername = $('#username'),
+    btnLogin = $('#login'),
+    btnLogout = $('#logout'),
+    chkActive = $('#active'),
+    nmbrInterval = $('#interval') 
+    chkReshedule = $('#reshedule'),
     txtContent = $('#content'),
-    tweetList = $('#tweets');
+    tweetList = $('#tweets'),
+    spnChars = $('#chars'),
+    birdcage = {};
 
-ref.authWithOAuthPopup("twitter", function(error, authData) {
-  if (error) {
-    console.log("Login Failed!", error);
-  } else {
-    console.log("Authenticated successfully with payload:", authData);
-    
-    ref.child("posts/" + authData.uid).orderByPriority().startAt(2).on("value", function(snapshot) {
+btnLogin.on('click', login);
+btnLogout.on('click', logout);
+btnTweet.on('click', tweet);
+chkActive.on('change', saveUserData);
+chkReshedule.on('change', saveUserData);
+nmbrInterval.on('blur', saveUserData);
+txtContent.on('keyup', updateLength);
+txtContent.on('blur', contentChanged);
+
+login();
+
+function login(){
+  ref.authWithOAuthPopup("twitter", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } 
+      else {
+        console.log("Authenticated successfully with payload:", authData);
+        spnUsername.text(authData.twitter.username);
+        birdcage.uid = authData.uid;
+        createOrLoadUser(authData);
+        loadPosts();    
+      }
+    }, 
+    {remember: "none"}
+    )
+}
+
+function createOrLoadUser(authData){
+    ref.child("users/" + birdcage.uid).once("value", function(snapshot){
+        console.log("Loaded user!");
+        var user = snapshot.val();
+        
+        if(!user){
+            console.log("Creating user!");
+            
+            user = {
+                access_token: authData.twitter.accessToken,
+                access_token_secret: authData.twitter.accessTokenSecret,
+                active: false,
+                reshedule: false,
+                uid: authData.uid
+            }
+            
+            ref.child('users/' + birdcage.uid).set(user);
+        }
+        birdcage.user = user;
+        chkActive.prop('checked', user.active);
+        chkReshedule.prop('checked', user.reshedule);
+    });
+}
+
+function saveUserData() {
+    ref.child("users/" + birdcage.uid).update({
+        active: chkActive.prop('checked'),
+        reshedule: chkReshedule.prop('checked'),
+        post_interval: nmbrInterval.val()
+        });
+}
+
+
+function loadPosts(){
+    ref.child("posts/" + birdcage.uid).orderByPriority().startAt(2).on("value", function(snapshot) {
     console.log(snapshot.val());
 
     tweetList.empty();
     var tweets = snapshot.val();
     $.each(tweets, function(id, tweet){
 
+        var upButton = $('<button>').text('up');
+        upButton.on('click', function(){alert('up')})
+
         $('<li>')
             .text(tweet.content)
+            .append(upButton)
             .appendTo(tweetList);
         })
     });
-   
-   
-   btnTweet.on('click', tweet);
+}
 
-    function tweet() {
-        ref.child('posts/' + authData.uid).push({
-                content: txtContent.val() + new Date(),
-                '.priority': Date.now(),
-                sent_count: 0
-            }
-        )
-    }
+function tweet() {
+    ref.child('posts/' + birdcage.uid).push({
+            content: txtContent.val() + new Date(),
+            '.priority': Date.now(),
+            sent_count: 0
+        }
+    )
+}
+
+function contentChanged(){
+    updateLength();
+    shorten();
+}
+
+function updateLength(){
+    var remaining = 140 - txtContent.val().length;
+    spnChars.text(remaining);
+}
+
+function shorten() {
+    var url = parseUrl()
     
-  }
-});
+    var bitly_login = "jbandi";
+    var bitly_api_key = "R_148e579c3fce39c2aac1d4bf4b38ac59";
+
+    if (url) {
+        $.ajax({
+            url: "http://api.bitly.com/v3/shorten?login=" + bitly_login + "&apiKey=" + bitly_api_key + "&longUrl=" + url,
+            dataType: "jsonp",
+            success: function(result) {
+                var short_url = result["data"]["url"];
+                if (short_url)
+                    replaceUrl(url, short_url)
+            }
+        })
+    }
+}
+
+function parseUrl() {
+        var bitly_url_regexp = /http:\/\/bit.ly\/[^\s]+/;
+        var url_match = txtContent.val().match(/(http:\/\/|https:\/\/|www\.)[^\s]+/g);
+
+        for (var i in url_match) {
+            var url = url_match[i];
+            if (!url.match(bitly_url_regexp))
+                return url;
+        }
+        return null;
+}
+
+function replaceUrl(url, short_url) {
+    var shortened_text = txtContent.val().replace(url, short_url);
+    txtContent.val(shortened_text);
+    updateLength();
+}
+
+function logout() {
+    ref.unauth();
+    loadPosts();
+}
+
 
 
 
