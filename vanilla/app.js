d9a1fcfb67e1d38d780b63fc0e65362f5872a0d2
Jonas Bandi <dev@jonasbandi.net>
2017-08-03 23:40:35 +0200

    vanilla

---
+++ vanilla/app.js
@@ -0,0 +1,155 @@
+FIREBASE_BASE = 'https://scorching-inferno-3523.firebaseio.com';
+
+// var ref = new Firebase("https://scorching-inferno-3523.firebaseio.com");
+
+
+
+var btnTweet = $('#tweet'),
+    spnUsername = $('#username'),
+    btnLogin = $('#login'),
+    btnLogout = $('#logout'),
+    chkActive = $('#active'),
+    nmbrInterval = $('#interval'),
+    chkReshedule = $('#reshedule'),
+    txtBitlyLogin = $('#bitly_login'),
+    txtBitlySecret = $('#bitly_secret'),
+    txtContent = $('#content'),
+    tweetList = $('#tweets'),
+    spnChars = $('#chars'),
+    btnRandomize = $('#randomize'),
+    chkShowAll = $('#showAll');
+
+
+birdcage2.init()
+    .then(renderUserData);
+
+birdcage2.onTweetsChange(renderTweets);
+
+btnLogin.on('click', login);
+btnLogout.on('click', logout);
+btnTweet.on('click', tweet);
+chkActive.on('change', saveUserData);
+chkReshedule.on('change', saveUserData);
+nmbrInterval.on('blur', saveUserData);
+txtBitlyLogin.on('blur', saveUserData);
+txtBitlySecret.on('blur', saveUserData);
+txtContent.on('keyup', updateLength);
+txtContent.on('blur', contentChanged);
+btnRandomize.on('click', randomize);
+chkShowAll.on('change', loadTweets);
+
+function renderUserData() {
+    var user = birdcage2.getUser();
+    spnUsername.text(user.username);
+    chkActive.prop('checked', user.active);
+    chkReshedule.prop('checked', user.reshedule);
+    nmbrInterval.val(user.post_interval);
+    txtBitlyLogin.val(user.bitly_login);
+    txtBitlySecret.val(user.bitly_secret);
+}
+
+function renderTweets(tweets) {
+
+    var scheduledTweets = tweets.filter(function(e){return e['.priority'] > 1});
+    var unscheduledTweets = tweets.filter(function(e){return e['.priority'] <= 1});
+
+    tweets = scheduledTweets.concat(unscheduledTweets);
+
+    tweetList.empty();
+    $.each(tweets, function (index, tweet) {
+
+        var li = $('<li>').html('(' + tweet['.priority'] + ') ' + Autolinker.link(tweet.content));
+
+        if (index > 0) {
+
+            var topButton = $('<button>').text('top');
+            topButton.on('click', function () {
+                birdcage2.topPriority(tweet);
+            });
+
+            var upButton = $('<button>').text('up');
+            upButton.on('click', function () {
+                birdcage2.increasePriority(tweet);
+            });
+
+            li.append(topButton);
+            li.append(upButton);
+        }
+
+        var deleteButton = $('<button>').text('delete');
+        deleteButton.on('click', function () {
+            birdcage2.deleteTweetById(tweet.id);
+        });
+        li.append(deleteButton);
+
+        li.appendTo(tweetList);
+    });
+}
+
+function saveUserData() {
+    var userData = {
+        active: chkActive.prop('checked'),
+        reshedule: chkReshedule.prop('checked'),
+        post_interval: nmbrInterval.val(),
+        bitly_login: txtBitlyLogin.val(),
+        bitly_secret: txtBitlySecret.val()
+    };
+
+    birdcage2.updateUser(userData)
+}
+
+function loadTweets(){
+    birdcage2.loadTweets(chkShowAll.prop('checked'));
+}
+
+function tweet() {
+    birdcage2.postTweet(txtContent.val());
+}
+
+function randomize(){
+    birdcage2.randomize();
+}
+
+function contentChanged() {
+    updateLength();
+    shorten();
+}
+
+function updateLength() {
+    var remaining = 140 - txtContent.val().length;
+    spnChars.text(remaining);
+}
+
+function shorten() {
+    var url = parseUrl()
+
+    if (url) {
+        $.ajax({
+            url: "http://api.bitly.com/v3/shorten?login=" + birdcage2.getUser().bitly_login + "&apiKey=" + birdcage2.getUser().bitly_secret + "&longUrl=" + url,
+            dataType: "jsonp",
+            success: function (result) {
+                var short_url = result["data"]["url"];
+                if (short_url)
+                    replaceUrl(url, short_url)
+            }
+        })
+    }
+}
+
+function parseUrl() {
+    var bitly_url_regexp = /http:\/\/bit.ly\/[^\s]+/;
+    var url_match = txtContent.val().match(/(http:\/\/|https:\/\/|www\.)[^\s]+/g);
+
+    for (var i in url_match) {
+        var url = url_match[i];
+        if (!url.match(bitly_url_regexp))
+            return url;
+    }
+    return null;
+}
+
+function replaceUrl(url, short_url) {
+    var shortened_text = txtContent.val().replace(url, short_url);
+    txtContent.val(shortened_text);
+    updateLength();
+}
---
+++ vanilla/src/birdcage.js
@@ -0,0 +1,209 @@
+(function (global) {
+    'use strict';
+
+    var config = {
+        apiKey: "AIzaSyDqq9FawE1g95hIK9UyBbqPYjCFFEOzzqM",
+        authDomain: "scorching-inferno-3523.firebaseapp.com",
+        databaseURL: "https://scorching-inferno-3523.firebaseio.com",
+        storageBucket: "scorching-inferno-3523.appspot.com"
+    };
+
+    firebase.initializeApp(config);
+
+
+    var FIREBASE_URL = 'https://scorching-inferno-3523.firebaseio.com',
+        // _firebase = new Firebase(FIREBASE_URL),
+        _firebase = firebase,
+        _authData = {},
+        _user = undefined,
+        _tweets = [],
+        _firebaseListener = undefined,
+        _tweetSubject = new Rx.ReplaySubject();
+
+
+    global.birdcage2 = {
+        init: init,
+        getAuthData: getAuthData,
+        getUser: getUser,
+        loadTweets: loadTweets,
+        updateUser: updateUser,
+        onTweetsChange: onTweetsChange,
+        postTweet: postTweet,
+        increasePriority: increasePriority,
+        topPriority: topPriority,
+        deleteTweetById: deleteTweetById,
+        randomize: randomize
+    };
+
+    function init() {
+        return new Promise(function (resolve, reject) {
+
+            var provider = new firebase.auth.TwitterAuthProvider();
+
+            firebase.auth().signInWithPopup(provider)
+                .then(function (result) {
+                // This gives you a the Twitter OAuth 1.0 Access Token and Secret.
+                // You can use these server side with your app's credentials to access the Twitter API.
+                var token = result.credential.accessToken;
+                var secret = result.credential.secret;
+                // The signed-in user info.
+                var user = result.user;
+
+                console.log("Authenticated successfully with payload:", token, secret, user);
+                _authData = user;
+                loadData();
+            }).catch(function (error) {
+                console.log(error);
+            });
+
+
+            // _authData = ref.getAuth();
+            // if (!_authData) {
+            // 	ref.authWithOAuthPopup("twitter", function (error, authData) {
+            // 	// ref.authWithOAuthRedirect("twitter", function (error, authData) {
+            // 		if (error) {
+            // 			console.log("Login Failed!", error);
+            // 		}
+            // 		else {
+            // 			console.log("Authenticated successfully with payload:", authData)
+            // 			_authData = authData;
+            // 			loadData();
+            // 		}
+            //
+            // 	});
+            // }
+            // else {
+            // 	loadData();
+            // }
+
+            function loadData() {
+                var userPromise = createOrLoadUser();
+                var tweetsPromise = loadTweets();
+
+                Promise.all([userPromise, tweetsPromise])
+                    .then(resolve)
+                    .catch(function () {
+                        console.log("An error occured while loading data!");
+                    })
+            }
+        })
+    }
+
+    function getAuthData() {
+        return _authData;
+    }
+
+    function getUser() {
+        return _user;
+    }
+
+    function updateUser(user) {
+        _firebase.database().ref('users/' + _authData.uid).update(user);
+    }
+
+    function onTweetsChange(handler) {
+        _tweetSubject.subscribe(handler);
+    }
+
+    function postTweet(text) {
+        var tweet = {
+            content: text,
+            '.priority': Date.now(),
+            sent_count: 0
+        };
+        _firebase.database().ref('posts/' + _authData.uid).push(tweet);
+    }
+
+    function increasePriority(tweet) {
+
+        if (tweet['.priority'] <= 1) {
+            // tweets with priority 1 are not scheduled
+            var highestPriority = _tweets[_tweets.length - 1]['.priority'];
+            _firebase.database().ref('posts/' + _authData.uid + '/' + tweet.id).update({'.priority': highestPriority + 1});
+        }
+        else {
+            var index = _tweets.indexOf(tweet);
+            if (index > 0) {
+                var prevPrio = _tweets[index - 1]['.priority'];
+                _firebase.database().ref('posts/' + _authData.uid + '/' + tweet.id).update({'.priority': prevPrio - 1});
+            }
+        }
+    }
+
+    function topPriority(tweet) {
+
+        var highestPriority = _tweets[0]['.priority'];
+        _firebase.database().ref('posts/' + _authData.uid + '/' + tweet.id).update({'.priority': highestPriority - 1});
+    }
+
+    function deleteTweetById(tweetId) {
+        _firebase.database().ref('posts/' + _authData.uid + '/' + tweetId).remove();
+    }
+
+
+    function randomize() {
+        $.each(_tweets, function (index, tweet) {
+            var randomPrio = Math.floor(Math.random() * 90000) + 10000
+            _firebase.database().ref('posts/' + _authData.uid + '/' + tweet.id).update({'.priority': randomPrio});
+        })
+    }
+
+    function createOrLoadUser() {
+
+        return new Promise(function (resolve, reject) {
+            debugger;
+            _firebase.database().ref("users/" + _authData.uid).once("value", function (snapshot) {
+
+                _user = snapshot.val();
+
+                if (!_user) {
+                    console.log("Creating new user!");
+                    _user = {
+                        access_token: _authData.twitter.accessToken,
+                        access_token_secret: _authData.twitter.accessTokenSecret,
+                        active: false,
+                        reshedule: false,
+                        uid: _authData.uid,
+                        username: _authData.twitter.username
+                    };
+                    ref.child('users/' + _authData.uid).set(_user, function () {
+                        console.log("Loaded created!");
+                        resolve();
+                    });
+                }
+                else {
+                    console.log("User loaded.");
+                    resolve();
+                }
+            });
+        });
+    }
+
+    function loadTweets(all) {
+        //all = true;
+        _firebaseListener && _firebaseListener.off();
+        _firebaseListener = _firebase.database().ref("posts/" + _authData.uid)
+            .orderByPriority()
+            .startAt(all ? 0 : 2);
+
+        return new Promise(function (resolve) {
+            _firebaseListener
+                .on("value", function (snapshot) {
+                    _tweets = [];
+                    var tweetsObj = snapshot.exportVal(); // also get priorities
+                    if (tweetsObj) {
+                        var tweetsArray = $.map(tweetsObj, function (value, id) {
+                            value.id = id;
+                            return value;
+                        });
+
+
+                        _tweets = tweetsArray;
+                    }
+                    resolve(); // resolving on any change ... not really elegant
+                    _tweetSubject.onNext(_tweets);
+                });
+        });
+    }
+
+})(window);
